abf29bfaf5fbc72b6a71f641140b323afee01f0c
abdul-kader138 <babu313136@gmail.com>
2017-09-27 09:57:37 +0600

    addOnClickEvent to Java Class

--- app/src/main/java/com/example/akader/myapplication/MainActivity.java
+++ app/src/main/java/com/example/akader/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.akader.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,11 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.b);
+//
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.et1);
+                TextView text = (TextView) findViewById(R.id.t2);
+                text.setText(editText.getText());
+            }
+        });
     }
 
-    public void loadText(View view){
-        EditText editText=(EditText) findViewById(R.id.et1);
-        TextView text=  (TextView) findViewById(R.id.t2);
+    public void loadText(View view) {
+        EditText editText = (EditText) findViewById(R.id.et1);
+        TextView text = (TextView) findViewById(R.id.t2);
         text.setText(editText.getText());
     }
 
