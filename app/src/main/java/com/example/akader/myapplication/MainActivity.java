989224d6a7430fb8335e4ce2de0b03c33bf4678e
abdul-kader138 <babu313136@gmail.com>
2017-09-27 14:20:02 +0600

    startActivityForResult() example

--- app/src/main/java/com/example/akader/myapplication/MainActivity.java
+++ app/src/main/java/com/example/akader/myapplication/MainActivity.java
@@ -1,46 +1,115 @@
 package com.example.akader.myapplication;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
+
+
+
+        /*
+        * get button id and add onclick listener to the button
+        * */
+
+
         Button button = (Button) findViewById(R.id.b);
         Button button2 = (Button) findViewById(R.id.b2);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText editText = (EditText) findViewById(R.id.et1);
                 Intent intent = new Intent();
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
+
+
+        /*
+        * ACTION_DIAL - send to dial with specific Number
+        * */
+
+                intent.setAction(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + editText.getText().toString()));
+
+
+                /*
+        * ACTION_GET_CONTENT - get any king of file like-Image,Music,Video etc
+        * */
+
+//                intent.setAction(Intent.ACTION_GET_CONTENT);
+//                intent.setType("image/*");
                 startActivity(intent);
             }
         });
 
 
+
+
+
+
+
+
+
+
+
+        /*
+        * get button id and add onclick listener to the button
+        * */
+
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 EditText editText1 = (EditText) findViewById(R.id.et1);
-                Intent intent1 = new Intent(getApplicationContext(), ScreenSecond.class);
+
+
+        /*
+        * Communicate between Activity to Activity
+        * startActivityForResult- here mainActivity send data to SecondActivity
+        * and also receive data from SecondActivity
+        * */
+                Intent intent1 = new Intent(MainActivity.this, ScreenSecond.class);
                 intent1.putExtra("val", editText1.getText().toString());
                 intent1.putExtra("val2", "Hello");
-                startActivity(intent1);
+                startActivityForResult(intent1, 2121);
             }
         });
+    }
+
+
+
+
+
+
+       /*
+        * onActivityResult- Check SecondActivity result data
+        * */
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 2121) {
+            TextView textView = (TextView) findViewById(R.id.t1);
+            String val = data.getStringExtra("Msg");
+            textView.setText(val);
+        }
+
     }
 }
 
--- app/src/main/java/com/example/akader/myapplication/ScreenSecond.java
+++ app/src/main/java/com/example/akader/myapplication/ScreenSecond.java
@@ -1,13 +1,12 @@
 package com.example.akader.myapplication;
 
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,10 +15,20 @@ public class ScreenSecond extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         TextView tv = new TextView(this);
         tv.setText("second activity");
         setContentView(R.layout.activity_second);
+
+
+
+
+          /*
+           * getIntent().getExtras() - get data from mainActivity;
+           * and show it at toast
+           * */
 
         Bundle bundle = getIntent().getExtras();
         Toast.makeText(getApplicationContext(), bundle.getString("val") + bundle.getString("val2"), Toast.LENGTH_LONG).show();
@@ -27,10 +36,20 @@ public class ScreenSecond extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                EditText editText = (EditText) findViewById(R.id.set1);
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
+
+                /*
+                *   Send result to mainActivity and finish the intent
+                *
+                * */
+                intent.putExtra("Msg", editText.getText().toString());
+                setResult(2121, intent);
+                finish();
+
 
             }
+
         });
     }
 
