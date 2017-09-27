9e7bbe765443c2f8c15e18da610da5577b4ce7dc
abdul-kader138 <babu313136@gmail.com>
2017-09-27 17:07:34 +0600

    Android DialogAlert() example

--- app/src/main/java/com/example/akader/myapplication/ScreenSecond.java
+++ app/src/main/java/com/example/akader/myapplication/ScreenSecond.java
@@ -1,12 +1,14 @@
 package com.example.akader.myapplication;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -14,19 +16,27 @@ public class ScreenSecond extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("second activity");
+
+
+
+        /*
+        * Check and work with Alert showing
+        * */
+
         setContentView(R.layout.activity_second);
 
 
 
 
-          /*
+
+
+
+        /*
            * getIntent().getExtras() - get data from mainActivity;
            * and show it at toast
            * */
 
-        Bundle bundle = getIntent().getExtras();
+        final Bundle bundle = getIntent().getExtras();
         Toast toast = Toast.makeText(getApplicationContext(), bundle.getString("val")
                         + bundle.getString("val2") + bundle.getString("chk") + bundle.getString("chk1"),
                 Toast.LENGTH_LONG);
@@ -48,5 +58,39 @@ public class ScreenSecond extends AppCompatActivity {
                 finish();
             }
         });
+
+
+        Button button2 = (Button) findViewById(R.id.bs1);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("test");
+                AlertDialog a = getAlertDialog(getApplicationContext()).create();
+                a.setTitle("Paragon Group");
+                a.show();
+            }
+        });
+
+
     }
+
+    private AlertDialog.Builder getAlertDialog(Context context) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getString(R.string.alertMsg)).
+                setCancelable(true).
+                setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        System.out.println("printing");
+//                        finish();
+                    }
+                }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        return builder;
+    }
+
 }
