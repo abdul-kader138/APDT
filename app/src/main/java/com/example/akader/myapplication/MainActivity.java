696ee3bc2f364754e55e0179169af12fd73d5fe2
abdul-kader138 <babu313136@gmail.com>
2017-09-27 09:26:24 +0600

    Understand XML & project structure doing simple text edit operation

--- app/src/main/java/com/example/akader/myapplication/MainActivity.java
+++ app/src/main/java/com/example/akader/myapplication/MainActivity.java
@@ -2,6 +2,12 @@ package com.example.akader.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loadText(View view){
+        EditText editText=(EditText) findViewById(R.id.et1);
+        TextView text=  (TextView) findViewById(R.id.t2);
+        text.setText(editText.getText());
     }
 
 
