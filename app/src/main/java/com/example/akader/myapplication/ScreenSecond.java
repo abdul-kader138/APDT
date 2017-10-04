cc5155e8a8b34ae368eb6e6aeecc33df2ac38aa4
abdul-kader138 <babu313136@gmail.com>
2017-10-04 16:40:58 +0600

    Calender View Example

--- app/src/main/java/com/example/akader/myapplication/ScreenSecond.java
+++ app/src/main/java/com/example/akader/myapplication/ScreenSecond.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RatingBar;
@@ -63,6 +65,25 @@ public class ScreenSecond extends AppCompatActivity {
 
             }
         });
+
+
+        /*
+        * Calender View
+        *
+        * */
+
+        CalendarView calendarView=(CalendarView) findViewById(R.id.cView);
+         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+             @Override
+             public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                 Toast.makeText(getApplicationContext(), "-" + i2+"/"+i1+"/"+i, Toast.LENGTH_LONG).show();
+             }
+         });
+
+
+
+
+
 
 
 
