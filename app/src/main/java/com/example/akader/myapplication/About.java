0b63b7e64a1bfbbdcf52c9fef703addcb9b863e6
abdul-kader138 <babu313136@gmail.com>
2017-10-05 16:58:32 +0600

    Android Tab View Example(Still need some fixing...)

---
+++ app/src/main/java/com/example/akader/myapplication/About.java
@@ -0,0 +1,18 @@
+package com.example.akader.myapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by a.kader on 10/5/2017.
+ */
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_layout);
+    }
+}
--- app/src/main/java/com/example/akader/myapplication/TabView.java
+++ app/src/main/java/com/example/akader/myapplication/TabView.java
@@ -1,24 +1,48 @@
 package com.example.akader.myapplication;
-import android.app.Activity;
+
+import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TabHost;
 import android.widget.TabHost.TabSpec;
+import android.widget.Toast;
 
 
-
-public class TabView extends Activity {
+public class TabView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tab_view);
-        TabHost tabHost=(TabHost) findViewById(R.id.tabHost);
-        TabSpec spec=tabHost.newTabSpec("Home");
-        spec.setIndicator("Tab");
-        Intent intent=new Intent(getApplicationContext(),Home.class);
+        TabHost tabHost = (TabHost) findViewById(android.R.id.tabhost); // initiate TabHost
+        TabHost.TabSpec spec; // Reusable TabSpec for each tab
+        Intent intent; // Reusable Intent for each tab
+
+
+        // Do the same for the other tabs
+
+        spec = tabHost.newTabSpec("HOME"); // Create a new TabSpec using tab host
+        spec.setIndicator("HOME"); // set the “CONTACT” as an indicator
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent(this, Home.class);
         spec.setContent(intent);
-        tabHost.setup();
         tabHost.addTab(spec);
+
+        spec = tabHost.newTabSpec("About"); // Create a new TabSpec using tab host
+        spec.setIndicator("ABOUT"); // set the “ABOUT” as an indicator
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent(this, About.class);
+        spec.setContent(intent);
+        tabHost.addTab(spec);
+        //set tab which one you want to open first time 0 or 1 or 2
+        tabHost.setCurrentTab(1);
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String tabId) {
+                // display the name of the tab whenever a tab is changed
+                Toast.makeText(getApplicationContext(), tabId, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
